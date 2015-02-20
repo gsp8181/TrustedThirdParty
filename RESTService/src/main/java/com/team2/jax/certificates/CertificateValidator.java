4c0ccbf0ea7fd1418bcbcc8a58bd9ad879eeddff
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-02-20 14:09:56 +0000

    Removed Test Repository Added a todo to validator

--- RESTService/src/main/java/com/team2/jax/certificates/CertificateValidator.java
+++ RESTService/src/main/java/com/team2/jax/certificates/CertificateValidator.java
@@ -3,8 +3,8 @@ package com.team2.jax.certificates;
 public class CertificateValidator {
 
 	public void validateCertificate(Certificate cert) {
-		return;
-		
+		//TODO: this method verifies that the certificate is valid, in date, an actual signature and if not
+		//Will throw an exception
 	}
 
 }
