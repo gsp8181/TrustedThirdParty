8de776682f1dcdf09e2d0dbf684ccb80eb4d96d4
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-02-20 16:24:00 +0000

    Dynamo Repo

---
+++ RESTService/src/main/java/com/team2/jax/certificates/CertificateRepositoryDynamo.java
@@ -0,0 +1,15 @@
+package com.team2.jax.certificates;
+
+public class CertificateRepositoryDynamo implements CertificateRepository {
+
+	public Certificate findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Certificate create(Certificate certificate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
