ae62d480bb8b8ae0b9037470e86f28bead3c77d9
rayearly <ray_early@hotmail.com>
2015-03-02 14:38:37 +0000

    ContractFileStoreS3 fixed, ContractFileStoreS3Test class to test S3

---
+++ RESTService/src/main/java/com/team2/jax/contract/ContractFileStoreS3Test.java
@@ -0,0 +1,17 @@
+package com.team2.jax.contract;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+public class ContractFileStoreS3Test {
+	
+	static ContractFileStoreS3 test = new ContractFileStoreS3();
+	
+	public static void main (String [] args) throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
+		byte[] doc = "0000".getBytes();
+		String fileName = "fileName";
+		String identifier = test.saveFile(fileName, doc);
+		System.out.println("Temporary URL: "+test.getTempLink(identifier));
+	}
+}
