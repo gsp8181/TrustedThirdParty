a1e1fdf347364c3773ffa99e76cfcd97a93ed6f3
atifnewcastle <atiframlan@gmail.com>
2015-03-02 15:13:03 +0000

    New JSONPostRequest

--- FrontEnd/src/main/java/com/team2/JsonPostRequest.java
+++ FrontEnd/src/main/java/com/team2/JsonPostRequest.java
@@ -42,30 +42,18 @@ public class JsonPostRequest {
 			             .add("number", "646 555-4567")))
 			     .build();
 		 
-		 
-		 
-		 JsonObject value = Json.createObjectBuilder()
-			     .add("firstName", "John")
-			     .add("lastName", "Smith")
-			     .add("age", 25)
-			     .add("address", Json.createObjectBuilder()
-			         .add("streetAddress", "21 2nd Street")
-			         .add("city", "New York")
-			         .add("state", "NY")
-			         .add("postalCode", "10021"))
-			     .add("phoneNumber", Json.createArrayBuilder()
-			         .add(Json.createObjectBuilder()
-			             .add("type", "home")
-			             .add("number", "212 555-1234"))
-			         .add(Json.createObjectBuilder()
-			             .add("type", "fax")
-			             .add("number", "646 555-4567")))
-			     .build();
 
 
 		OutputStreamWriter wr= new OutputStreamWriter(con.getOutputStream());
 //		wr.write(parent.toString());
 
+	}
+	
+	
+	public static void test(){
+		
+		
+		
 	}
 
 }
