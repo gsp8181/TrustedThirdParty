4c5d2359ae5bee29a1acf68b9bfe8c9c4151fb46
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-03-03 18:07:50 +0000

    Removed plain exception throwing on certificate create

--- RESTService/src/main/java/com/team2/jax/certificates/CertificateService.java
+++ RESTService/src/main/java/com/team2/jax/certificates/CertificateService.java
@@ -23,7 +23,7 @@ public class CertificateService {
 		return crud.findByEmail(email);
 	}
 
-	public Certificate create(CertificateIn cert) throws ConstraintViolationException, ValidationException, Exception {
+	public Certificate create(CertificateIn cert) throws ConstraintViolationException, ValidationException {
 		
 		validator.validateCertificate(cert);	
 		
