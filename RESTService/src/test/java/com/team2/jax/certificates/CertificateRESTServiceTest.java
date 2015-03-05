19642713fb601e3aef97ab1115d57e985b73a7bf
rayearly <ray_early@hotmail.com>
2015-03-05 11:15:18 +0000

    test certificate

---
+++ RESTService/src/test/java/com/team2/jax/certificates/CertificateRESTServiceTest.java
@@ -0,0 +1,45 @@
+package com.team2.jax.certificates;
+
+import static com.jayway.restassured.RestAssured.*;
+//import groovyx.net.http.ContentType;
+
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
+
+//import javax.ws.rs.core.Response.Status;
+
+import static org.junit.Assert.*;
+import com.team2.jax.certificates.CertificateRESTService;
+
+//import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CertificateRESTServiceTest {
+	
+	static CertificateRESTService cf = new CertificateRESTService();				
+	
+	@Test
+	public void getCertByUsernameTest() {
+		cf.getCertByUsername("rayearly@gmail.com");
+		
+		Response res = get("http://localhost:8080/service/rest/certificates/rayearly@gmail.com");
+		
+		String json = res.asString();
+		
+		JsonPath jp = new JsonPath(json);
+		
+		assertEquals("rayearly@gmail.com", jp.get("email"));
+	}
+	
+
+	/*@Test
+	public void sendCertTest() {
+		assertNull(null);
+	}*/
+	
+	/*@Test
+	public void verifyEmailTest() {
+		assertNull(null);
+	}*/
+	
+}
